b51436b6b595d2409a36773f637bfc5d8eac2eae
GustavoMori <gustavomoripeixoto@hotmail.com>
2021-09-27 14:25:53 -0300

    refactory of it on test exercise 2

--- spec/exercise_2_spec.rb
+++ spec/exercise_2_spec.rb
@@ -2,27 +2,28 @@ require_relative '../exercises/exercise_2'
 
 describe 'make_upper_case' do
   context 'When args are strings' do
-    it 'If argument is in lower case' do 
-      expect(make_upper_case('hidemi')).to eq('HIDEMI')
+    context 'If argument is in lower case' do
+      it 'transform a string to a upper case string' do 
+        expect(make_upper_case('hidemi')).to eq('HIDEMI')
+      end
     end
-
-    it 'If any letter in string already in upper case' do
+    
+    it 'return all letters in upper case' do
       expect(make_upper_case('hIdemi')).to eq('HIDEMI')
     end
 
-    it 'If all characters in string already in upper case' do
+    it 'return all letters in upper case' do
       expect(make_upper_case('HIDEMI')).to eq('HIDEMI')
     end
 
-    it 'If any characters in string is special characters or numbers' do
+    it 'return all letters in upper case and ignore numbers and symbols' do
       expect(make_upper_case('hidemi741$*')).to eq('HIDEMI741$*')
     end
   end
 
   context 'When args arent strings' do
-    it 'If the argument is a number' do
-      expect {make_upper_case(1234)} .to raise_error(NoMethodError)
+    it 'return a NoMethodError' do
+      expect {make_upper_case(1234)}.to raise_error(NoMethodError)
     end
   end
-
 end
